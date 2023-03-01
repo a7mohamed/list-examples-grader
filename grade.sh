0712f2fb583a8b6b8ce93cac830ec8be44fcb178
a7mohamed <a7mohamed@ucsd.edu>
2023-03-01 12:50:55 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,6 @@
-CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar *.java'
 
+rm -rf tests/
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
@@ -15,6 +16,21 @@ else
     exit 1
 fi
 
-cp ../TestListExamples.java ./
+cd ..
+mkdir ./tests
+cp ./student-submission/ListExamples.java ./tests
+cp ./TestListExamples.java ./tests
+
+cd tests
+
 javac -cp $CPATH *.java
-java TestListExamples
+
+if [ $? -ne 0 ]; then
+echo "Compilation failed. Try again."
+exit 1
+fi
+
+java -cp CPATH org.junit.runner.JUnitCore TestListExamples | grep . > tests_results.txt
+
+
+
