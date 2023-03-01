ab8a14e31cd09c8bfc33ee6b4eaccc9eb134f8a4
a7mohamed <a7mohamed@ucsd.edu>
2023-03-01 11:12:50 -0800

    Not done yet!

--- grade.sh
+++ grade.sh
@@ -1,5 +1,20 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+EXPECT='ListExamples.java'
+
+if [[ -f $EXPECT ]]
+then
+    echo "ListExamples found!"
+else
+    echo "Need file ListExamples!"
+    exit 1
+fi
+
+cp ../TestListExamples.java ./
+javac -cp $CPATH *.java
+java TestListExamples
